622e729405165ef7fc48a3393617ad9422cb5df6
abibats <1186738544@qq.com>
2025-02-24 11:06:40 +0800

    111

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'campus', 'publications']
+const section_names = ['home', 'awards', 'campus', 'projects']
 
 
 window.addEventListener('DOMContentLoaded', event => {
